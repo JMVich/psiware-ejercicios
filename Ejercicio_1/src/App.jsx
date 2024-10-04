f44d1c88e8e80eec5c31845041c0e581785655f8
Juan Mateo Viscovich <juanviscovich14@gmail.com>
2024-10-04 19:57:19 -0300

    Resolución del ejercicio 1 - B.

--- Ejercicio_1/src/App.jsx
+++ Ejercicio_1/src/App.jsx
@@ -3,27 +3,39 @@ import './App.css'
 
 function App() {
 
+    let array = [1, 10, 6, 8, 15, 2];  // Creamos el array con los números
+
     //  1) A)
 
-        let array = [1, 10, 6, 8, 15, 2];  // Creamos el array con los números
-
-        let mayor = 0;  // Creamos la variable auxiliar "mayor" (número más grande)
-        let pos = 0;  // Creamos la variable auxiliar "pos" (posición del número más grande)
-
-        // Recorremos el array llamado "array"
-        for (let i = 0; i < array.length; i++) {
-
-            // Calculamos si "mayor" es menor que el número actual
-            if (mayor < array[i]) {
-                mayor = array[i];  // Guardamos el número más grande
-                pos = i;  // Guardamos la posición del número más grande
+            /* 
+            let mayor = 0;  // Creamos la variable auxiliar "mayor" (número más grande)
+            let pos = 0;  // Creamos la variable auxiliar "pos" (posición del número más grande)
+    
+            // Recorremos el array llamado "array"
+            for (let i = 0; i < array.length; i++) {
+    
+                // Calculamos si "mayor" es menor que el número actual
+                if (mayor < array[i]) {
+                    mayor = array[i];  // Guardamos el número más grande
+                    pos = i;  // Guardamos la posición del número más grande
+                }
             }
-        }
-        
-        // Mostramos en consola el número más grande y su posición (mayor = 15, posición = 4)
-        console.log(`El número más grande es ${mayor} y está en la posición ${pos}`);
+            
+            // Mostramos en consola el número más grande y la posición (mayor = 15, posición = 4)
+            console.log(`Número mayor: ${mayor} / Posición: ${pos}`); 
+            */
 
     //  1) B)
+
+            /*             
+            const mayor = Math.max(...array);  // Buscamos el número más grande
+
+            const pos = array.indexOf(mayor);  // Buscamos la posición del número más grande
+
+            // Mostramos en consola el número más grande y la posición (mayor = 15, posición = 4)
+            console.log(`Número mayor: ${mayor} / Posición: ${pos}`);  
+            */
+
 
     return (
         <>
